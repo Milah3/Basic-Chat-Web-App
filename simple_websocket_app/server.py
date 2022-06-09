aade44c089ed87c1177fa16cc52f727aeb965534
Cigomba Miler <cigombamiler@gmail.com>
2022-06-08 23:08:12 -0700

    finally got the websocket connection to work

--- simple_websocket_app/server.py
+++ simple_websocket_app/server.py
@@ -1,43 +1,47 @@
+import threading
 from flask import Flask, request, redirect, render_template, url_for
 import simple_websocket
+# from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+# CORS(app)
+# app.config['CORS_HEADERS'] = 'Content-Type'
+# cors = CORS(app, )
 
 
 @app.route('/echo', websocket=True)
+# @cross_origin()
 def echo():
     ws = simple_websocket.Server(request.environ)
-    if ws.connected:
-        msg = ws.receive()
-        print(msg)
-        print('IT worked. Connection successful!')
-        ws.close()
-    return redirect(url_for('home'))
-
-    # ws.send('Hello there!!!')
     try:
         while True:
             msg = ws.receive()
+            # data = [msg, username]
             print(msg)
+            ws.send(msg)
+            pass
     except simple_websocket.ConnectionClosed:
+        print('socket connection closed!')
+        # pass
+    except Exception as e:
+        print('ERROR:', e)
         pass
     return ''
 
 
 @app.route('/')
+@app.route('/home')
 def home():
     return render_template('index.html')
 
 
-@app.route('/run', methods=["GET", "POST"])
-def handleData():
-    # if request.method == 'POST':
-    #     msg = request.args['msg']
-    #     print(f'Message received: {msg}')
-    #     return 'data received successfully!'
-    # return 'method was not post'
-    pass
-
+# def set_interval(func, sec):
+#     def func_wrapper():
+#         set_interval(func, sec)
+#         func()
+#     t = threading.Timer(sec, func_wrapper)
+#     t.start()
+#     return t
 
 if __name__ == '__main__':
     app.run(debug=True)
--- simple_websocket_app/static/run.js
+++ simple_websocket_app/static/run.js
@@ -1,20 +1,9 @@
 // $(document).ready(function () {
-//     $('#send').bind('click', function () {
+//     $('#send').bind('click', function() {
 //         const message = document.getElementById('msg');
-//         console.log(message);
-//         // $(document).load('/run', { 'msg': message.value }, function () { message.value = ''; return 'run complete' });
-
+//         console.log(message.value, 'sent!');
+//         $(document).load('/run', { 'msg': message.value }, function () { message.value = ''; return 'run complete' });
+        
 //     });
 // });
 
-// const socket = new WebSocket('ws://localhost:5000/echo');
-// socket.send('This is my message. It works')
-// fetch(`${window.origin}/run`, {
-//     method: "POST",
-//     credentials: "include",
-//     body: JSON.stringify(message.value),
-//     cache: "no-cache",
-//     headers: new Headers({
-//         "content-type": "application/json"
-//     })
-// })
